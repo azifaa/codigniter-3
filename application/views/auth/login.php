305ea66b7a2df8b62a7df3aa72675ebf40a3bb3f
azifaa <dwiazifatuljannah@gmail.com>
2023-09-13 15:35:58 +0700

    codigniter

---
+++ application/views/auth/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <style>
+body { 
+        background-image: url("https://wallpapercave.com/wp/wp4981488.jpg"); 
+    }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+<body>
+  <div class="container ">
+
+    <div class="card mt-5 w-50 justify-content-center mx-auto bg-transparent">
+      <h5 class="card-header mx-auto text-white text-bold">Login</h5>
+      <div class="card-body">
+        <form action="connect_login.php" method="post">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label text-white text-bold " placeholder="Email Address" >Email address</label>
+            <input name="email" type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text text-white text-bold">We'll never share your email with anyone else.</div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label text-white text-bold" placeholder="password">Password</label>
+            <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="password">
+          </div>
+          <div class="d-grid gap-2 col-6 mx-auto">
+          <button type="submit" class="btn btn-primary text-bg-info text-white text-bold">LOGIN</button>
+        </form>
+        
+      </div>
+    </div>
+  </div>
+</div>
+  </body>
+  </html>
