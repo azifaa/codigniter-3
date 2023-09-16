3896df5d1cb937c0cf77c8879f224670070f76d3
azifaa <dwiazifatuljannah@gmail.com>
2023-09-16 12:05:30 +0700

    membuat fungsi update

--- application/views/admin/tambah_siswa.php
+++ application/views/admin/tambah_siswa.php
@@ -2,48 +2,69 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <meta name="viewport" content="width=<div class="container mt-12>
+    <script src="https://cdn.tailwindcss.com"></script>
+ 
+ <div class="overflow-x-auto"> 
+     <form action="<?php echo base_url('admin/aksi_Tambah_siswa') ?>" enctype="multipart/form-data" 
+         method="post"> 
+         <div class="max-full rounded border overflow-hidden shadow-lg"> 
+             <div class="px-6 py-4"> 
+                 <p class="text-xl font-bold text-center">Tambah Siswa</p> 
+                 <div class="grid grid-cols-2 gap-4 mt-5"> 
+                     <div class="mb-4"> 
+                         <label class="block text-gray-700 text-sm font-bold mb-2" for="nama"> 
+                             Nama Siswa 
+                         </label> 
+                         <input 
+                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                             id="nama" name="nama" type="text" placeholder="Nama"> 
+                     </div> 
+                     <div class="mb-4"> 
+                         <label class="block text-gray-700 text-sm font-bold mb-2" for="nisn"> 
+                             NISN 
+                         </label> 
+                         <input 
+                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                             id="nisn" name="nisn" type="number" placeholder="Nisn"> 
+                     </div> 
+                     <div class="mb-4"> 
+                         <label class="block text-gray-700 text-sm font-bold mb-2" for="gender"> 
+                             Gender 
+                         </label> 
+                         <select name="gender" id="gender" 
+                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"> 
+                             <option selected>Pilih Gender</option> 
+                             <option value="Laki-Laki">Laki-Laki</option> 
+                             <option value="Perempuan">Perempuan</option> 
+                         </select> 
+                     </div> 
+                     <div class="mb-4"> 
+                                    <label class="block text-gray-700 text-sm font-bold mb-2" for="kelas"> 
+                                        Kelas 
+                                    </label> 
+                                    <select name="kelas" id="kelas" 
+                                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"> 
+                                        <option selected>Pilih Kelas</option> 
+                                        <?php foreach($kelas as $row): ?> 
+                                        <option value="<?php echo $row->id_kelas ?>"> 
+                                            <?php echo $row->tingkat_kelas.' '.$row->jurusan_kelas ?></option> 
+                                        <?php endforeach ?> 
+                                    </select> 
+                                </div>
+                     <button type="submit" 
+                         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-2/6"> 
+                         Tambah 
+                     </button> 
+                 </div> 
+             </div> 
+         </div> 
+     </form> 
+ </div> 
+</div> 
+</div>
 </head>
-<body class="min-vh-100 d-flex align-items-center">
-    <div class="card w-50 m-auto p-3">
-        <h3 class="text-center">Tambah Data</h3>
-        <form action="<?php echo base_url('admin/aksi_tambah_siswa') ?>"
-        enctype="multipart/form-data"
-        method="post" class="row">
-            <div class="mb-3 col-6">
-                <label for="nama" class="form-label">Nama Siswa</label>
-                <input type="text" class="form-control" id="nama" name="nama">
-            </div>
-            <div class="mb-3 col-6">
-                <label for="nisn" class="form-label">NISN</label>
-                <input type="text" class="form-control" id="nisn" name="nisn">
-            </div>
-            <div class="mb-3 col-6">
-                <label for="gender" class="form-label">Gender</label>
-                <select name="gender" class="form-select">
-                    <option selected>Pilih Gender</option>
-                    <option value="Laki-Laki">Laki-Laki</option>
-                    <option value="Perempuan">Perempuan</option>
-                </select>
-            </div>
-            <div class="mb-3 col-6">
-                <label for="kelas" class="form-label">Kelas</label>
-                <select name="id_kelas" class="form-select">
-                    <option selected>Pilih Kelas</option>
-                    <?php foreach($kelas as $row): ?>
-                    <option value="<?php echo $row->id?>">
-                        <?php echo $row->tingkat_kelas.' '.$row->jurusan_kelas?>
-                    </option>
-                    <?php endforeach ?>
-                </select>
-                <div class="d-grid gap-2 d-md-block">
-                <button class="btn btn-primary" type="submit">Submit</button>
-                </div>
-                </div>
-        </form>
-    </div>
+<body>
+    
 </body>
-
 </html>
